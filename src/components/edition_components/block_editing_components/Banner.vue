f262d75ae6d34a0d75fc03f7b95248c70b7ce55b
antoitoo01 <antonio123444@hotmail.com>
2024-05-07 18:07:55 +0200

    Cambios decorativos

--- src/components/edition_components/block_editing_components/Banner.vue
+++ src/components/edition_components/block_editing_components/Banner.vue
@@ -1,15 +1,24 @@
 <script setup>
+import { ref } from "vue";
+
+const url = ref("");
 </script>
 
 <template>
-  <div class="size-full flex flex-col place-items-center p-3">
+  <div class="size-full flex flex-col place-items-center p-4">
     <h2 class="font-bold">Banner</h2>
     <div class="divider" />
+    <p class="input-title">Url de la imagen:</p>
+    <input class="custom-input text-sm" v-model="url" />
   </div>
 </template>
 
 <style scoped>
 .divider {
-  @apply bg-slate-400 w-[91%] min-w-7 h-[1px] opacity-75;
+  @apply bg-slate-400 w-[91%] min-w-7 h-[1px] opacity-75 z-0;
+}
+
+.input-title {
+  @apply mt-4 mb-2 self-start text-sm font-semibold;
 }
 </style>
