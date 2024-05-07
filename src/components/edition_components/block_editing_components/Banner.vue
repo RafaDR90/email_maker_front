94af63611fc454334e4d14d6838b916d5f62e42c
antoitoo01 <antonio123444@hotmail.com>
2024-05-07 18:30:49 +0200

    Fin Jornada

--- src/components/edition_components/block_editing_components/Banner.vue
+++ src/components/edition_components/block_editing_components/Banner.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { ref } from "vue";
+import Slider from "./Slider.vue";
+import ColorPicker from "./ColorPicker.vue";
 
 const url = ref("");
 </script>
@@ -8,17 +10,25 @@ const url = ref("");
   <div class="size-full flex flex-col place-items-center p-4">
     <h2 class="font-bold">Banner</h2>
     <div class="divider" />
-    <p class="input-title">Url de la imagen:</p>
+    <!-- Img URL -->
+    <h3>Url de la imagen:</h3>
     <input class="custom-input text-sm" v-model="url" />
+
+    <!-- Slider -->
+    <h3>Margen inferior:</h3>
+    <Slider />
+
+    <!-- Background -->
+    <h3>Fondo:</h3>
+    <ColorPicker/>
   </div>
 </template>
 
 <style scoped>
+h3 {
+  @apply mt-4 mb-2 self-start text-sm font-semibold;
+}
 .divider {
   @apply bg-slate-400 w-[91%] min-w-7 h-[1px] opacity-75 z-0;
-}
-
-.input-title {
-  @apply mt-4 mb-2 self-start text-sm font-semibold;
 }
 </style>
