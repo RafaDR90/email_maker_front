fc7e3437778ff3335d22efdcb8461d4c2b34c986
antoitoo01 <antonio123444@hotmail.com>
2024-05-07 17:32:32 +0200

    post-merging

--- src/components/edition_components/BlockEdit.vue
+++ src/components/edition_components/BlockEdit.vue
@@ -1,10 +1,12 @@
 <script setup>
+import Banner from "./block_editing_components/Banner.vue";
+
 
 </script>
 
 
 <template>
     <div class=" w-[25%] bg-yellow-300">
-        <h1>block edit</h1>
+        <Banner/>
     </div>
 </template>
