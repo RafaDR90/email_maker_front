9add64f419efb81bba8d75c7787036ea84ec7dca
antoitoo01 <antonio123444@hotmail.com>
2024-05-14 18:29:05 +0200

    Super guay

--- src/components/edition_components/EmailPreview.vue
+++ src/components/edition_components/EmailPreview.vue
@@ -86,32 +86,6 @@ const downloadHTMLPrueba = () => {
   a.click();
   URL.revokeObjectURL(url);
 };
-
-/**
- * Función para comprobar si la imagen es una URL
- */
-function isURL(newUrl) {
-  try {
-    const url = new URL(newUrl);
-    return url.protocol === "http:" || url.protocol === "https:";
-  } catch (error) {
-    return false;
-  }
-}
-
-/**
- * Función para comprobar si la imagen es válida.
- */
-function isImageValid(imageURL) {
-  if (!isURL(imageURL)) {
-    console.log("a");
-    return false;
-  }
-  const img = new Image();
-  img.src = imageURL;
-
-  return img.complete && img.naturalWidth !== 0;
-}
 </script>
 
 
@@ -169,6 +143,7 @@ function isImageValid(imageURL) {
             backgroundColor: emailVarsStore.bgColor || '#FFFFFF',
           }"
         >
+          <!-- Top Banner -->
           <NewBanner
             :bannerImageUrl="bannerStore.bannerUrl"
             :bannerBackgroundColor="bannerStore.bannerColor"
@@ -176,65 +151,7 @@ function isImageValid(imageURL) {
             @click="updateSelectedBlock('banner')"
             class="selectable-block"
           />
-          <!-- Top Banner -->
-          <!--
-          <div @click="updateSelectedBlock('banner')" class="selectable-block">
-            <div
-              v-if="!bannerStore.bannerUrl"
-              :style="{
-                backgroundColor: bannerStore.bannerColor || '',
-                paddingBottom: bannerStore.marginBottom + 'px',
-              }"
-              style="
-                width: 100%;
-                height: fit-content;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-              "
-            >
-              <div
-                style="
-                  display: flex;
-                  flex-direction: column;
-                  justify-content: center;
-                  align-items: center;
-                "
-              >
-                <img
-                  style="width: 5rem; height: 5rem"
-                  :src="Banner"
-                  alt="imagen banner"
-                />
 
-                <p style="font-size: 2.25rem; color: #6b7280">Inserte imagen</p>
-              </div>
-            </div>
-            <div
-              v-else
-              :style="{
-                backgroundColor: bannerStore.bannerColor,
-                paddingBottom: bannerStore.marginBottom + 'px',
-              }"
-            >
-              <img
-                v-if="isImageValid(bannerStore.bannerUrl)"
-                style="width: 100%"
-                :src="bannerStore.bannerUrl"
-                alt="imagen banner"
-              />
-              <div
-                v-else
-                style="width: 100%; padding: 20px; background-color: lightcoral"
-                align="center"
-              >
-                <p style="color: darkred; font-weight: bold">
-                  ¡Imagen no encontrada!
-                </p>
-              </div>
-            </div>
-          </div>
-          -->
           <!-- Top Banner Text-->
           <div
             @click="updateSelectedBlock('underBannerText')"
